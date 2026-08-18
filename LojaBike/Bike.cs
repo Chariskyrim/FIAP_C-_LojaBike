41f08e41da63a7e5a16541398993fda7ec2feadb
chariskyrim <enzolucianox@gmail.com>
2026-08-17 21:59:11 -0300

    feat: Criando declarações e funções da pasta

--- LojaBike/Bike.cs
+++ LojaBike/Bike.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LojaBike
+﻿namespace LojaBike
 {
-    internal class Bike
+    class Bike
     {
+        //DECLARAÇÕES
+        public string Modelo { get; set; } = string.Empty;
+        public double Bateria { get; set; }
+        public double PesoCiclista { get; set; }
+        public int RecargaMes { get; set; }
+
+        //MÉTODOS
+        public double CalcularAutonomia()
+        {
+            double autonomiaBase = Bateria / 15.0;
+            bool pesoElevado = PesoCiclista > 90.0;
+            //SE O PESO FOR ACIMA DE 90KH, A BATERIA REDUZ PARA 15% DE AUTONOMIA
+            return pesoElevado ?(autonomiaBase * 0.05) : autonomiaBase; //Estrutura condicional (If = ?, Else = :)
+
+        }
+
+        public double CalcularConsumoMensal()
+        {
+            double kmPorCarga = Bateria / 1000.00;
+            return kmPorCarga * RecargaMes;
+
+        }
+
+        public double CalcularCustoMensal()
+        {
+            const double PRECO_KWH = 0.80; // Preço do kWh em reais
+            return CalcularConsumoMensal() * PRECO_KWH;
+        }
+
+        public bool ModeloEconomico()
+        {
+            return (CalcularCustoMensal() < 15.00) || (CalcularAutonomia() > 40.0);
+        }
     }
 }
